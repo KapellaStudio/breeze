1604b81beac3056e07987d85c47937fc4b2f6784
Kapella Venture Studio <goldenmonkeyindex@gmail.com>
2026-08-23 20:20:39 -0400

    Inject real omnibox suggestions into packaged Breeze

--- build.py
+++ build.py
@@ -32,6 +32,7 @@ vaultui  = _read(SRC / 'breeze-vault.js')
 homeui   = _read(SRC / 'breeze-home.js')
 runtime  = _read(SRC / 'breeze-runtime.js')
 searchui = _read(SRC / 'breeze-search-ui.js')
+omniui   = _read(SRC / 'breeze-omni.js')
 
 def _b64_text(*paths):
     for path in paths:
@@ -67,7 +68,7 @@ for name in SHELLS:
         if '</body>' not in html:
             print(f'  !! {name}: missing </body> for desktop module injection'); fail = True
         else:
-            modules = ''.join('<script>\n' + module + '\n</script>\n' for module in (launch, product, context, vaultui, homeui, runtime, searchui))
+            modules = ''.join('<script>\n' + module + '\n</script>\n' for module in (launch, product, context, vaultui, homeui, runtime, searchui, omniui))
             html = html.replace('</body>', modules + '</body>', 1)
     html = html.replace('data:image/png;base64,__LOGO__', 'data:image/svg+xml;base64,' + logo_svg_b64)
     html = html.replace('__LOGO__', logo_svg_b64)
